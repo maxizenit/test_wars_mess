d2c03eb09a4fce880fb4159d36343d53eada3733
Maxim Puzanov <maxizenit@gmail.com>
2025-03-18 15:03:51 +0300

    Completed tests

--- src/test/java/org/itmo/SpyMessengerTest.java
+++ src/test/java/org/itmo/SpyMessengerTest.java
@@ -1,19 +1,68 @@
 package org.itmo;
 
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class SpyMessengerTest {
+
+    private SpyMessenger messenger;
+
+    @BeforeEach
+    void initMessenger() {
+        messenger = new SpyMessenger();
+    }
+
     @Test
     void testMessageSelfDestructionAfterReading() {
-        SpyMessenger messenger = new SpyMessenger();
         messenger.sendMessage("Alice", "Bob", "Top Secret", "1234");
 
-        // Первое чтение — сообщение должно быть доступно
         assertEquals("Top Secret", messenger.readMessage("Bob", "1234"));
+        assertNull(messenger.readMessage("Bob", "1234"));
+
+        messenger.sendMessage("Alice", "Bob", "1", "1234");
+        messenger.sendMessage("Alice", "Bob", "2", "1234");
+
+        assertEquals("1", messenger.readMessage("Bob", "1234"));
+        assertEquals("2", messenger.readMessage("Bob", "1234"));
     }
+
+    @Test
+    void testMessageSelfDestructionAfterDelay() throws InterruptedException {
+        messenger.sendMessage("Alice", "Bob", "Top Secret", "1234");
+        Thread.sleep(2000);
+
+        assertNull(messenger.readMessage("Bob", "1234"));
+
+        messenger.sendMessage("Alice", "Bob", "1", "1234");
+        Thread.sleep(800);
+        messenger.sendMessage("Alice", "Bob", "2", "1234");
+        Thread.sleep(800);
+
+        assertEquals("2", messenger.readMessage("Bob", "1234"));
+    }
+
+    @Test
+    void testWrongPasscode() {
+        messenger.sendMessage("Alice", "Bob", "Top Secret", "1234");
+
+        assertNull(messenger.readMessage("Bob", "2345"));
+    }
+
+    @Test
+    void testClearRedundantMessages() {
+        messenger.sendMessage("Alice", "Bob", "1", "1234");
+        messenger.sendMessage("Alice", "Bob", "2", "1234");
+        messenger.sendMessage("Alice", "Bob", "3", "1234");
+        messenger.sendMessage("Alice", "Bob", "4", "1234");
+        messenger.sendMessage("Alice", "Bob", "5", "1234");
+        messenger.sendMessage("Alice", "Bob", "6", "1234");
+        messenger.sendMessage("Alice", "Bob", "7", "1234");
+
+        assertEquals("3", messenger.readMessage("Bob", "1234"));
+    }
+
 
 }
